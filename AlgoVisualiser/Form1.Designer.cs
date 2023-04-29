6b75beb21b351dca4d35a668f62ca365e9347436
lilsteelan <stellan.lindrud@gmail.com>
2023-04-29 13:31:19 +1000

    Content

---
+++ AlgoVisualiser/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace AlgoVisualiser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.Reset = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.visualiserPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(182)))));
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.Reset);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-4, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(195, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(38, 419);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(112, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Descending";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(38, 389);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(102, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ascending";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Reset
+            // 
+            this.Reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reset.Location = new System.Drawing.Point(38, 523);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(103, 34);
+            this.Reset.TabIndex = 7;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(38, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 34);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Quick Sort";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(38, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Bubble Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(38, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Insertion Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(16, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Made By Stellan Lindrud";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(4)))), ((int)(((byte)(94)))));
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AlgoVisualiser";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.visualiserPanel);
+            this.panel2.Location = new System.Drawing.Point(186, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(679, 600);
+            this.panel2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(279, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(188, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(306, 46);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Algorithm Name";
+            // 
+            // visualiserPanel
+            // 
+            this.visualiserPanel.Location = new System.Drawing.Point(11, 137);
+            this.visualiserPanel.Name = "visualiserPanel";
+            this.visualiserPanel.Size = new System.Drawing.Size(655, 450);
+            this.visualiserPanel.TabIndex = 0;
+            this.visualiserPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.visualiserPanel_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 599);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "AlgoVisualiser - Stellan";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel visualiserPanel;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ AlgoVisualiser/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace AlgoVisualiser
+{
+    public partial class Form1 : Form
+    {
+        public static int iteration;
+        public Form1()
+        {
+            InitializeComponent();
+            List<int> numbers = generateList(32);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Insertion Sort";
+            InsertionSort();
+            visualiserPanel.Refresh();
+            renderList();
+        }
+
+        private void visualiserPanel_Paint(object sender, PaintEventArgs e)
+        {
+            renderList();
+            Console.WriteLine("panel was called");
+        }
+
+        public void renderList()
+        {
+            int listLength = m_numbers.Count();
+            int objHeight;
+            int objWidth = 10;
+            int posX = 10;
+            int posY = 215;
+            int offsetX = 20;
+            SolidBrush sbBlack = new SolidBrush(Color.Black);
+            SolidBrush sbGray = new SolidBrush(Color.Gray);
+            Graphics g = visualiserPanel.CreateGraphics();
+
+            for (int i = 0; i < listLength; i++)
+            {
+                objHeight = m_numbers[i];
+                //tmp += 1;
+                if (i % 2 == 0)//Check if even
+                {
+                    g.FillRectangle(sbBlack, posX, (posY + ((450 - posY) - objHeight)), objWidth, objHeight); //brush, x,y,sizex,sizey
+                    posX += offsetX;
+                }
+                else //Else change Color
+                {
+                    g.FillRectangle(sbGray, posX, (posY + ((450 - posY) - objHeight)), objWidth, objHeight);
+                    posX += offsetX;
+                }
+            }
+        }
+
+        public List<int> m_numbers;
+        public bool m_descending = false;
+        public bool m_isSorting = false;
+
+        List<int> generateList(int listLength)
+        {
+            //Create a list of numbers
+            Random rnd = new Random();
+            m_numbers = new List<int>();
+            for (int i = 0; i < listLength; i++)
+            {
+                m_numbers.Add(rnd.Next(60, 450));
+            }
+            return m_numbers;
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            List<int> numbers = generateList(32);
+            visualiserPanel.Refresh();
+            renderList();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (m_isSorting)
+            {
+                //
+            }
+            else
+            {
+                m_descending = true;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Bubble Sort";
+            BubbleSort();
+            visualiserPanel.Refresh();
+            renderList();
+        }
+
+        void BubbleSort()
+        {
+            //foreach (var number in m_numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            Console.WriteLine("------------");
+            
+            bool swapRequired;
+            var n = m_numbers.Count();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            Graphics g = visualiserPanel.CreateGraphics();
+            if (m_isSorting)
+            {
+
+            }
+            else
+            {
+                m_isSorting = true;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - (1 + i); j++)
+                    {
+                        swapRequired = false;
+                        Refresh();
+                        g.FillEllipse(sb, 10 + (20 * j), 10, 20, 20);
+                        wait(15);
+                        if (!m_descending)
+                        {
+                            if (m_numbers[j] > m_numbers[j + 1])
+                            {
+                                var tempVar = m_numbers[j + 1];
+                                m_numbers[j + 1] = m_numbers[j];
+                                m_numbers[j] = tempVar;
+                                swapRequired = true;
+                            }
+                        }
+                        if (m_descending)
+                        {
+                            if (m_numbers[j] < m_numbers[j + 1])
+                            {
+                                var tempVar = m_numbers[j + 1];
+                                m_numbers[j + 1] = m_numbers[j];
+                                m_numbers[j] = tempVar;
+                                swapRequired = true;
+                            }
+                        }
+                        if (swapRequired == false)
+                        {
+                            //break;
+                        }
+
+                    }
+                }
+                m_isSorting = false;
+            }
+            
+        }
+
+        void InsertionSort()
+        {
+            
+            SolidBrush sb = new SolidBrush(Color.Red);
+            Graphics g = visualiserPanel.CreateGraphics();
+            if (m_isSorting)
+            {
+
+            }
+            else
+            {
+                m_isSorting = true;
+                for (int i = 0; i < m_numbers.Count(); i++)
+                {
+                    int current = m_numbers[i];
+
+                    int j = i - 1;
+                    if (m_descending)
+                    {
+                        while (j >= 0 && current > m_numbers[j])
+                        {
+                            m_numbers[j + 1] = m_numbers[j];
+                            j -= 1;
+                            Refresh();
+
+                            g.FillEllipse(sb, 10 + (10 * j), 10, 20, 20);
+                            wait(15);
+                        }
+                        m_numbers[j + 1] = current;
+                    }
+                    if (!m_descending)
+                    {
+                        while (j >= 0 && current < m_numbers[j])
+                        {
+                            m_numbers[j + 1] = m_numbers[j];
+                            j -= 1;
+                            Refresh();
+                            g.FillEllipse(sb, 10 + (10 * j), 10, 20, 20);
+                            wait(15);
+                        }
+                        m_numbers[j + 1] = current;
+                    }
+                }
+                m_isSorting = false;
+            }
+        }
+
+        void Quicksort(int left, int right)
+        {
+            
+            SolidBrush sb = new SolidBrush(Color.Red);
+            Graphics g = visualiserPanel.CreateGraphics();
+            int i = left;
+            int j = right;
+
+            var pivot = m_numbers[(left + right) / 2];
+            if (!m_descending)
+            {
+                m_isSorting = true;
+                while (i <= j)
+                {
+                    Refresh();
+                    g.FillEllipse(sb, 10 + (10 * j), 10, 20, 20);
+                    wait(15);
+                    while (m_numbers[i] < pivot)
+                        i++;
+
+                    while (m_numbers[j] > pivot)
+                        j--;
+
+                    if (i <= j)
+                    {
+                        var tmp = m_numbers[i];
+                        m_numbers[i] = m_numbers[j];
+                        m_numbers[j] = tmp;
+
+                        i++;
+                        j--;
+                    }
+                }
+
+                if (left < j)
+                    Quicksort(left, j);
+
+                if (i < right)
+                    Quicksort(i, right);
+            }
+            if (m_descending)
+            {
+                m_isSorting = true;
+                while (i <= j)
+                {
+                    Refresh();
+                    g.FillEllipse(sb, 10 + (10 * j), 10, 20, 20);
+                    wait(15);
+                    while (m_numbers[i] > pivot)
+                        i++;
+
+                    while (m_numbers[j] < pivot)
+                        j--;
+
+                    if (i <= j)
+                    {
+                        var tmp = m_numbers[i];
+                        m_numbers[i] = m_numbers[j];
+                        m_numbers[j] = tmp;
+
+                        i++;
+                        j--;
+                    }
+                }
+
+                if (left < j)
+                    Quicksort(left, j);
+
+                if (i < right)
+                    Quicksort(i, right);
+            }
+            m_isSorting = false;
+        }
+
+        void Sleep(int waitTime) //Given in ms
+        {
+            DateTime time = DateTime.Now;
+            int initialTime = time.Millisecond;
+            while (true)
+            {
+                DateTime timetmp = DateTime.Now;
+                int currentTime = timetmp.Millisecond;
+                if(currentTime - initialTime >= waitTime)
+                {
+                    return;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        public void Refresh()
+        {
+            visualiserPanel.Refresh();
+        }
+
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            // Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Console.WriteLine("stop wait timer");
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(m_isSorting == true)
+            {
+
+            }
+            else
+            {
+                m_descending = false;
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Quick Sort";
+            Quicksort(0, m_numbers.Count()-1);
+            visualiserPanel.Refresh();
+            renderList();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
